4f323896a768b5225ae99cd12e43f1048f242d73
ariefkahfi <ariefkahfi@gmail.com>
2023-01-18 17:25:10 +0700

    update script

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.post('/calculate', (req, res) => {
   return res.status(200).json(jsonData)
 })
 
-app.get('/', (req, res) => res.json({ data: "Hello World updated" }))
+app.get('/', (req, res) => res.json({ data: "Hello World updated 2" }))
 
 module.exports = app
